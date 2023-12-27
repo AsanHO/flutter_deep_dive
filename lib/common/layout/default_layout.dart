96e0f3836f540c002b04d5a2615356fbff4dbfe0
AsanHO <gorma00@naver.com>
2023-12-27 21:03:16 +0900

    RootTab TabBarView 생성

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -1,31 +1,44 @@
 import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
+  final Color? backgroundColor;
   final Widget child;
-  final Color backgroundColor;
+  final String? title;
+  final Widget? bottomNavigationBar;
 
   const DefaultLayout({
-    super.key,
     required this.child,
-    this.backgroundColor = Colors.white,
-  });
+    this.backgroundColor,
+    this.title,
+    this.bottomNavigationBar,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: backgroundColor,
-      body: SafeArea(
-        bottom: false,
-        child: Center(
-          child: SingleChildScrollView(
-            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: child,
-            ),
-          ),
+      backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
+      body: child,
+      bottomNavigationBar: bottomNavigationBar,
+    );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    }
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      title: Text(
+        title!,
+        style: TextStyle(
+          fontSize: 16.0,
+          fontWeight: FontWeight.w500,
         ),
       ),
+      foregroundColor: Colors.black,
     );
   }
 }
