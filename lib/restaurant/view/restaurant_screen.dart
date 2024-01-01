ab419a06805834175963185b317dc5d8f9229ba6
AsanHO <gorma00@naver.com>
2024-01-01 14:35:49 +0900

    레스토랑 스크린 api 통신 연결

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,26 +1,59 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_deep_dive/common/const/environments.dart';
 import 'package:flutter_deep_dive/restaurant/components/restaurant_card.dart';
 
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({Key? key}) : super(key: key);
+
+  Future<List> getRestaurant() async {
+    final dio = Dio();
+
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    final resp = await dio.get(
+      '$IP/restaurant',
+      options: Options(
+        headers: {'authorization': 'Bearer $accessToken'},
+      ),
+    );
+
+    return resp.data['data'];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal:16.0),
-          child: RestaurantCard(
-            image: Image.asset(
-              'asset/img/food/ddeok_bok_gi.jpg',
-              fit: BoxFit.cover,
-            ),
-            name: '불타는 떡볶이',
-            tags: ['떡볶이', '치즈', '매운맛'],
-            ratingCount : 100,
-            deliveryTime : 15,
-            deliveryFee : 2000,
-            rating : 4.52,
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: FutureBuilder(
+            future: getRestaurant(),
+            builder: (context, AsyncSnapshot<List> snapshot) {
+              if (!snapshot.hasData) {
+                return Container();
+              }
+              return ListView.separated(
+                  itemBuilder: (_, idx) {
+                    final item = snapshot.data![idx];
+                    return RestaurantCard(
+                      image: Image.network(
+                        '$IP${item['thumbUrl']}',
+                        fit: BoxFit.cover,
+                      ),
+                      name: item['name'],
+                      tags: List<String>.from(item['tags']),
+                      ratingsCount: item['ratingsCount'],
+                      deliveryTime: item['deliveryTime'],
+                      deliveryFee: item['deliveryFee'],
+                      ratings: item['ratings'],
+                    );
+                  },
+                  separatorBuilder: (_, idx) {
+                    return Divider();
+                  },
+                  itemCount: snapshot.data!.length);
+            },
           ),
         ),
       ),
