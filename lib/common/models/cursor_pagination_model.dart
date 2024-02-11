46590221e09c341ae1e431273f9332eadbef609c
AsanHO <gorma00@naver.com>
2024-02-11 13:34:46 +0900

    restaurantProvider 작성 및 교체
    pagenation model 재구축, base및 상속 클래스 작성

--- lib/common/models/cursor_pagination_model.dart
+++ lib/common/models/cursor_pagination_model.dart
@@ -2,10 +2,34 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'cursor_pagination_model.g.dart';
 
-@JsonSerializable(
-  genericArgumentFactories: true
-)
-class CursorPagination<T> {
+abstract class CursorPaginationBase {}
+
+class CursorPaginationError extends CursorPaginationBase {
+  final String message;
+
+  CursorPaginationError({required this.message});
+}
+
+class CursorPaginationLoading extends CursorPaginationBase {}
+
+//새로고침을 했을 때 CursorPagination 상속
+class CursorPaginationRefetching extends CursorPagination {
+  CursorPaginationRefetching({
+    required super.data,
+    required super.meta,
+  });
+}
+
+//추가 페이지 요청,
+class CursorPaginationFetchingMore extends CursorPagination {
+  CursorPaginationFetchingMore({
+    required super.data,
+    required super.meta,
+  });
+}
+
+@JsonSerializable(genericArgumentFactories: true)
+class CursorPagination<T> extends CursorPaginationBase {
   final CursorPaginationMeta meta;
   final List<T> data;
 
