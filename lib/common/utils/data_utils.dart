019d08019ed51b2b5234c981662e45e06678177f
AsanHO <gorma00@naver.com>
2024-02-13 23:36:03 +0900

    별점 카드 컴포넌트
    모델 생성

--- lib/common/utils/data_utils.dart
+++ lib/common/utils/data_utils.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_deep_dive/common/const/environments.dart';
 
 class DataUtils{
-  static pathToUrl(String value) {
+  static String pathToUrl(String value) {
     return '$IP/$value';
+  }
+
+  static List<String> ListPathToUrls(List<String> paths) {
+    return paths.map((e) => pathToUrl(e)).toList();
   }
 }
---
+++ lib/restaurant/models/rating_model.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_deep_dive/common/utils/data_utils.dart';
+import 'package:flutter_deep_dive/user/models/user_model.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'rating_model.g.dart';
+
+@JsonSerializable()
+class RatingModel {
+  final String id;
+  final UserModel user;
+  final int rating;
+  final String content;
+  @JsonKey(
+    fromJson: DataUtils.ListPathToUrls,
+  )
+  final List<String> images;
+
+  RatingModel({
+    required this.id,
+    required this.user,
+    required this.rating,
+    required this.content,
+    required this.images,
+  });
+
+  factory RatingModel.fromJson(Map<String, dynamic> json) =>
+      _$RatingModelFromJson(json);
+}
