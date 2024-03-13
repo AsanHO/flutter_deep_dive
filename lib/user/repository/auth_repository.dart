224db197c6851b9338ed61e68c8f2a2e9c58eed2
AsanHO <gorma00@naver.com>
2024-03-13 21:13:02 +0900

    로그인, 토큰 리프레시 레포지토리

---
+++ lib/user/repository/auth_repository.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_deep_dive/common/const/environments.dart';
+import 'package:flutter_deep_dive/common/models/login_response.dart';
+import 'package:flutter_deep_dive/common/models/token_response.dart';
+
+class AuthRepository {
+  final String baseUrl;
+  final Dio dio;
+
+  AuthRepository({
+    required this.baseUrl,
+    required this.dio,
+  });
+
+  Future<LoginResponse> login({
+    required String email,
+    required String password,
+  }) async {
+    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+
+    String token = stringToBase64.encode('$email:$password');
+    final resp = await dio.post(
+      "$IP/auth/login",
+      options: Options(
+        headers: {'authorization': 'Basic $token'},
+      ),
+    );
+
+    return LoginResponse.fromJson(resp.data);
+  }
+
+  Future<TokenResponse> token() async {
+    final resp = await dio.post('$baseUrl/token',
+        options: Options(headers: {'refreshToken': 'true'}));
+    return TokenResponse.fromJson(resp.data);
+  }
+}
